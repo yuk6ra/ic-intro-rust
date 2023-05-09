cacf13b169ac13dba7aae74479ee2874fcade4f8
yuk6ra <yuki.imamura.all@gmail.com>
2023-05-09 21:35:23 +0900

    Initial commit.

---
+++ src/gm_world_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { gm_world_backend } from "../../declarations/gm_world_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await gm_world_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
